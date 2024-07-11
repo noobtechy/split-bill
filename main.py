c7c56cb3d151cb6fadccb52fef7fe891245dbfbb
piyush yadav <mufa1026@gmail.com>
2024-07-11 23:43:07 +0530

    basic bill splitting

---
+++ main.py
@@ -0,0 +1,7 @@
+n=int(input("write the total member going :  "))
+a=int(input("total money used in event :  "))
+avg = a/n
+for j in range(0,n):
+    memb = input("write the name of the person: ")
+    print (memb,"-",avg)
+    
